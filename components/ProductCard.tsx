b8f517fa6dcbf998ef0110c61ebd9f24a6ddb1ac
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-02 16:25:16 -0400

    add offer overlay

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -18,7 +18,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   }
 
   return (
-    <div className='bg-white border-2 border-[--bg-300] rounded-xl p-3 w-64'>
+    <div className='bg-white border-2 border-[--bg-300] rounded-xl p-3 w-64 relative overflow-hidden'>
+      {
+        product.discount > 0 && (
+          <div className='absolute -top-2 -left-12 bg-[--primary-100] text-[--text-200] font-bold rounded-lg px-14 py-4 -rotate-45'>
+            -{product.discount}%
+          </div>
+        )
+      }
       <div className='w-full flex items-center justify-center mb-4'>
         <Image
           src={product.image[0]}
@@ -27,16 +34,22 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           height={200}
           />
       </div>
-      <p className='text-[--text-100] font-medium text-base'>
+      <p className='text-[--text-100] font-medium text-base cursor-pointer'>
         {product.brand} - {product.name}
       </p>
-      <p className='text-xs text-[--primary-300] pt-1 pb-3'>
+      <p className='text-xs text-[--primary-300] mt-2 mb-4 cursor-pointer'>
         {product.brand}
       </p>
       <div className='flex gap-2 mt-2 mb-6'>
         {
           product.weightOptions.map(option => (
-            <p className={`text-[--text-100] text-sm px-2 rounded-lg py-1 cursor-pointer transition-all ${option.weight === selectedWeight.weight ? 'bg-[--accent-100] text-[--bg-300]' : 'bg-[--bg-300] text-[--text-100]'}}`}
+            <p className={`
+              text-[--text-100] text-sm px-2 rounded-lg py-1 cursor-pointer transition-all select-none
+              ${option.weight === selectedWeight.weight
+                ? 'bg-[--accent-100]'
+                : 'bg-[--bg-300] text-[--text-100] hover:bg-[--accent-200] hover:text-[--bg-100]'
+              }
+            `}
             key={option.weight}
               onClick={() => { handleWeightChange(option.weight) }}>
               {option.weight}
@@ -47,7 +60,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <p className='text-[--text-200] font-semibold text-lg'>
         ${selectedWeight.price.toLocaleString('es-CL')}
       </p>
-      <button className='p-2 my-2 w-full bg-[--accent-100] text-[--text-200] rounded-lg transition-all hover:bg-[--accent-200]'>
+      <button className='p-2 my-2 w-full bg-[--accent-100] text-[--text-200] rounded-lg transition-all hover:bg-[--accent-200] hover:text-[--bg-100]'>
         Agregar
       </button>
     </div>
