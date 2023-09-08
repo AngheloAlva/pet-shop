a15be5ad074dc192a510aa0d87e5f37a034c9af2
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-08 18:38:18 -0300

    add two href

--- data/NavbarItems.ts
+++ data/NavbarItems.ts
@@ -1,97 +1,97 @@
 export const dogMenu = [
   {
     title: 'ALIMENTOS PARA PERRO',
-    href: '',
+    href: 'category/64f222ee811be8e9842f15f1',
     description: ['Humedo', 'Seco', 'Cachorro', 'Adulto', 'Senior', 'Todas las Edades', 'Prescripcion Veterinaria']
   },
   {
     title: 'BAÑOS SANITARIOS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Pañal de Adiestramiento']
   },
   {
     title: 'PLATOS Y DISPENSADORES',
-    href: '',
+    href: 'category/',
     description: ['Fuentes de Agua', 'Alimentadores Automaticos', 'Plato Alimentacion Lenta', 'Contenedor de Alimento']
   },
   {
     title: 'SALUD Y BIENESTAR',
-    href: '',
+    href: 'category/',
     description: ['Antiparasitario Externo', 'Antiparasitario Interno', 'Suplementos Nutricionales', 'Accesorios Post Operatorios', 'Tranquilizantes']
   },
   {
     title: 'SNACK PARA PERROS',
-    href: '',
+    href: 'category/',
     description: ['Huesos', 'Galletas', 'Cuidado Dental', 'Snack Deshidratado', 'Premios', 'Snack Cremoso']
   },
   {
     title: 'CAMAS Y CASAS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Camas', 'Casas', 'Mantas Refrescantes', 'Mantas Termicas']
   },
   {
     title: 'PUERTAS ABATIBLES PARA PERRO',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'TRANSPORTADORES PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Corrales', 'Bolsos de Transporte', 'Mochila de Transporte', 'Jaula de Transporte']
   },
   {
     title: 'JUGUETES PARA PERROS',
-    href: '',
+    href: 'category/',
     description: ['Juguete con Sonido', 'Pelota', 'Peluche', 'Juguete con Cuerda', 'Juguete Dental Porta Snack', 'Juguete con Sabor', 'Juguete Resistente']
   },
   {
     title: 'ARTICULOS DE PELUQUERIA',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ACCESORIOS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ARNES PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Arnes con Correa']
   },
   {
     title: 'COLLARES PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Collar de Adiestramiento']
   },
   {
     title: 'CORREAS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: ['Correas con Collar']
   },
   {
     title: 'BOZAL PARA PERROS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ROPA DE PERRO',
-    href: '',
+    href: 'category/',
     description: ['Ropa de Invierno']
   },
   {
     title: 'CEPILLOS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'CUIDADO E HIGIENE',
-    href: '',
+    href: 'category/',
     description: ['Shampoo y Acondicionador', 'Colonia', 'Cuidado Dental', 'Maquinas de Cortar pelo']
   },
   {
     title: 'SERVICIO/GRABADO DE PLACAS PARA PERRO',
-    href: '',
+    href: 'category/',
     description: []
   }
 ]
@@ -99,82 +99,82 @@ export const dogMenu = [
 export const catMenu = [
   {
     title: 'ALIMENTOS PARA GATO',
-    href: '',
+    href: 'category/64f22453811be8e9842f15f4',
     description: ['Humedo', 'Seco', 'Kitten', 'Adulto', 'Senior', 'Todas las edades', 'Prescripcion Veterinaria']
   },
   {
     title: 'CAMAS Y CASAS PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Camas', 'Casas', 'Mantas Refrescantes', 'Mantas Termicas']
   },
   {
     title: 'ROPA PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Ropa de Invierno']
   },
   {
     title: 'SNACK PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Galletas', 'Snack Cremoso', 'Snack Deshidratado']
   },
   {
     title: 'BAÑOS Y ARENAS PARA GATO',
-    href: '',
+    href: 'category/',
     description: ['Kit Sanitario', 'Arenas Sanitarias', 'Baños Sanitarios']
   },
   {
     title: 'JUGUETES PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Varilla', 'Ratones', 'Peluches', 'Juguetes con Catnip']
   },
   {
     title: 'CEPILLOS PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'TRANSPORTADORAS PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Bolsos de Transporte', 'Mochila de Transporte', 'Jaula de Transporte', 'Corral para Gatitos']
   },
   {
     title: 'ACCESORIOS PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ARNES PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'COLLARES PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'RASCADORES PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Rascador Pequeño', 'Rascador Mediano', 'Rascador Grande']
   },
   {
     title: 'PLATOS Y DISPENSADORES',
-    href: '',
+    href: 'category/',
     description: ['Fuentes de Agua', 'Alimentadores Automaticos', 'Plato Alimentacion Lenta', 'Contenedor de Alimento']
   },
   {
     title: 'CUIDADO E HIGIENE PARA GATOS',
-    href: '',
+    href: 'category/',
     description: ['Shampoo y Acondicionador', 'Maquinas de Cortar pelo']
   },
   {
     title: 'PUERTAS ABATIBLES PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'SERVICIO DE GRABADO DE PLACAS PARA GATOS',
-    href: '',
+    href: 'category/',
     description: []
   }
 ]
@@ -182,47 +182,47 @@ export const catMenu = [
 export const aquaristicMenu = [
   {
     title: 'ALIMENTO PARA PECES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ALIMENTO PARA TORTUGAS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ACUARIOS PARA PECES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ACCESORIOS PARA ACUARIO',
-    href: '',
+    href: 'category/',
     description: ['Adornos']
   },
   {
     title: 'CALEFACCION PARA PECES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'FILTROS PARA ACUARIOS',
-    href: '',
+    href: 'category/',
     description: ['Bombas de Aire', 'Filtros Internos', 'Filtros Externos']
   },
   {
     title: 'ILUMINACION PARA ACUARIOS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'TORTUGEROS',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'CUIDADO E HIGIENE PARA PECES',
-    href: '',
+    href: 'category/',
     description: []
   }
 ]
@@ -230,27 +230,27 @@ export const aquaristicMenu = [
 export const birdMenu = [
   {
     title: 'ALIMENTO PARA AVES',
-    href: '',
+    href: 'category/',
     description: ['Snacks']
   },
   {
     title: 'ACCESORIOS PARA AVES',
-    href: '',
+    href: 'category/',
     description: ['Bañeras', 'Columpios', 'Juguetes', 'Nidos']
   },
   {
     title: 'JAULAS PARA AVES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'COMEDEROS Y BEBEDEROS PARA AVES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'CUIDADO E HIGIENE PARA AVES',
-    href: '',
+    href: 'category/',
     description: []
   }
 ]
@@ -258,32 +258,32 @@ export const birdMenu = [
 export const smallAnimalMenu = [
   {
     title: 'CONEJOS',
-    href: '',
+    href: 'category/',
     description: ['Jaula y Corral', 'Accesorios y Juguetes', 'Transportadoras', 'Baño Sanitario y Viruta', 'Alimento', 'Comedero y Bebedero', 'Salud y Bienestar', 'Cuidado e Higiene', 'Camas y Casas']
   },
   {
     title: 'COBAYOS',
-    href: '',
+    href: 'category/',
     description: ['Jaula y Corral para Cuy', 'Camas y Casas', 'Transportadoras', 'Alimento', 'Comedero y Bebedero', 'Baño Sanitario y Viruta', 'Accesorios y Juguetes', 'Salud y Bienestar', 'Cuidado e Higiene']
   },
   {
     title: 'HAMSTER / JERBOS Y RATAS',
-    href: '',
+    href: 'category/',
     description: ['Jaula para Hamster', 'Camas y Casas', 'Baño Sanitari y Viruta', 'Ruedas y Esferas', 'Alimento', 'Transportadora', 'Comedero y Bebedero', 'Salud y Bienestar', 'Cuidado e Higiene', 'Accesorios y Juguetes', 'Tubos para Hamster']
   },
   {
     title: 'HURONES',
-    href: '',
+    href: 'category/',
     description: ['Jaula y Corral para Huron', 'Alimento', 'Transportadora', 'Comedero y Bebedero', 'Viruta', 'Cuidado e Higiene', 'Accesorios y Juguetes']
   },
   {
     title: 'ERIZOS',
-    href: '',
+    href: 'category/',
     description: ['Jaula y Corral para Erizo', 'Ruedas', 'Alimento', 'Transportadora', 'Comedero y Bebedero', 'Viruta', 'Cuidado e Higiene', 'Placas y Ampolletas Calefactoras', 'Accesorios y Juguetes']
   },
   {
     title: 'CHINCHILLAS',
-    href: '',
+    href: 'category/',
     description: ['Alimento', 'Comedero y Bebedero', 'Baño Sanitario y Viruta', 'Cuiddado e Higiene', 'Accesorios y Juguetes', 'Jaulas y Corral']
   }
 ]
@@ -291,22 +291,22 @@ export const smallAnimalMenu = [
 export const reptileMenu = [
   {
     title: 'ACCESORIOS PARA REPTILES',
-    href: '',
+    href: 'category/',
     description: ['Adornos', 'Comederos', 'Cuevas', 'Pinzas']
   },
   {
     title: 'CALEFACCION PARA REPTILES',
-    href: '',
+    href: 'category/',
     description: ['Ampolletas y Lamparas', 'Placas Calefactoras', 'Humidificadores']
   },
   {
     title: 'TERRARIOS PARA REPTILES',
-    href: '',
+    href: 'category/',
     description: []
   },
   {
     title: 'ALIMENTO PARA REPTILES',
-    href: '',
+    href: 'category/',
     description: []
   }
 ]
