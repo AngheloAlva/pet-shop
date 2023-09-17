7f5e9569050b13d39c719f357a59e7b3c9423623
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-16 21:02:33 -0300

    add dropdown for login/logout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,24 +12,14 @@ import SheetMenu from './nav/SheetMenu'
 
 import { catMenu, dogMenu, aquaristicMenu, birdMenu, reptileMenu, smallAnimalMenu } from '@/data/NavbarItems'
 import NavMenuItem from './NavMenuItem'
+import UserDropdown from './nav/User-dropdown'
+import Search from './nav/Search'
 
 import {
-  FaCircleUser,
   FaBagShopping
 } from 'react-icons/fa6'
-import Search from './nav/Search'
-import { useUser } from '@auth0/nextjs-auth0/client'
 
 const NavBar = (): JSX.Element => {
-  const { user, error, isLoading } = useUser()
-  console.log(user, error, isLoading)
-
-  const signInButton = (
-    <div className='flex gap-2 items-center'>
-      <FaCircleUser className='text-3xl text-[--bg-100] cursor-pointer p-[.1rem] rounded-full border-[--bg-100] border-2 hover:text-[--bg-200] hover:border-[--bg-200] transition-colors' />
-    </div>
-  )
-
   return (
     <nav className='flex flex-col bg-[--bg-100]'>
       <header className='flex justify-between items-center px-2 py-1 bg-[--accent-100] border-b-2 border-[--accent-100]'>
@@ -43,8 +33,7 @@ const NavBar = (): JSX.Element => {
         <div className='flex gap-2 items-center'>
           <Search />
           <FaBagShopping className='text-3xl text-[--bg-100] cursor-pointer p-[.1rem] rounded-full border-[--bg-100] border-2 hover:text-[--bg-200] hover:border-[--bg-200] transition-colors' />
-          <a href="/api/auth/login">{signInButton}</a>
-          <a href="/api/auth/logout">Logout</a>
+          <UserDropdown />
         </div>
       </header>
 
---
+++ components/nav/User-dropdown.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { FaCircleUser, FaUser, FaArrowRightFromBracket } from 'react-icons/fa6'
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuLabel
+} from '@/components/ui/dropdown-menu'
+
+const UserDropdown = (): JSX.Element => {
+  const { user } = useUser()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <FaCircleUser className='text-3xl text-[--bg-100] cursor-pointer p-[.1rem] rounded-full border-[--bg-100] border-2 hover:text-[--bg-200] hover:border-[--bg-200] transition-colors' />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className='text-[--text-200]'>
+          {
+            user === undefined
+              ? ''
+              : (
+                  <div className='flex gap-2 items-center'>
+                    <img src={user.picture ?? ''} alt={`Foto de perfil de ${user.name}` ?? ''} className='rounded-full w-7' />
+                    {user.name}
+                  </div>
+                )
+          }
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {
+          user === undefined
+            ? (
+              <DropdownMenuItem>
+                <Link href="/api/auth/login" className='flex gap-2 text-[--text-200] hover:text-[--accent-200] transition-all w-full'>
+                  <FaUser />
+                  <p>Iniciar Sesion</p>
+                </Link>
+              </DropdownMenuItem>
+              )
+            : (
+              <>
+                <DropdownMenuItem>
+                  <Link href="/profile" className='flex gap-2 text-[--text-200] hover:text-[--accent-200] transition-all w-full'>
+                    <FaUser />
+                    <p>Perfil</p>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/api/auth/logout" className='flex gap-2 text-[--text-200] hover:text-[--accent-200] transition-all w-full'>
+                    <FaArrowRightFromBracket />
+                    <p>Cerrar Sesion</p>
+                  </Link>
+                </DropdownMenuItem>
+              </>
+              )
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdown
