c2d0282b314ebfc00c74ca7d2f0208cf6e187527
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-22 19:53:39 -0300

    install react-email

---
+++ react-email-starter/.react-email/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,24 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\app\\preview\\[slug]\\page": [
+      "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\styles\\globals.css",
+      "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\app\\page": [
+      "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\styles\\globals.css",
+      "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\app\\layout.tsx": [
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ],
+  "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\app\\preview\\[slug]\\page.tsx": [
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ],
+  "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\app\\page.tsx": [
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "C:\\Users\\ancea\\Desktop\\pet-shop\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ]
+}
