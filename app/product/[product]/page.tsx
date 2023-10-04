e6e458216c26975003f520434597cb9cfbc6eb8c
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-04 17:18:13 -0300

    fix style and fix quantity problems

--- app/product/[product]/page.tsx
+++ app/product/[product]/page.tsx
@@ -1,18 +1,20 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
-import { getProductById } from '@/api/product'
-import type { Product } from '@/interfaces/interfaces'
+import Link from 'next/link'
 import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
 import { FaCartPlus, FaShop, FaTruckFast, FaShieldHalved, FaTruckArrowRight } from 'react-icons/fa6'
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/ui/accordion'
-import Link from 'next/link'
+
+import { getProductById } from '@/api/product'
+import type { Product } from '@/interfaces/interfaces'
 
 export default function ProductView ({ params }: { params: { product: string } }): JSX.Element {
-  const [product, setProduct] = useState<Product>()
-  const [optionSelected, setoptionSelected] = useState<Product['options'][0]>()
   const [quantity, setQuantity] = useState(1)
+  const [product, setProduct] = useState<Product>()
   const [selectedImage, setSelectedImage] = useState(0)
+  const [optionSelected, setoptionSelected] = useState<Product['options'][0]>()
 
   useEffect(() => {
     getProductById(params.product)
@@ -23,19 +25,20 @@ export default function ProductView ({ params }: { params: { product: string } }
       .catch((error) => { console.log(error) })
   }, [params.product])
 
-  const handleWeightChange = (option: string): void => {
+  const handleOptionChange = (option: string): void => {
     const optionSelected = product?.options.find(op => op.option === option)
 
     if (optionSelected != null) {
       setoptionSelected(optionSelected)
+      setQuantity(1)
     }
   }
 
-  // const increaseQuantity = (): void => {
-  //   if (product?.stock !== undefined && quantity < product?.stock) {
-  //     setQuantity(quantity + 1)
-  //   }
-  // }
+  const increaseQuantity = (): void => {
+    if (optionSelected?.stock !== undefined && quantity < Number(optionSelected.stock)) {
+      setQuantity(quantity + 1)
+    }
+  }
 
   const decreaseQuantity = (): void => {
     if (quantity > 1) {
@@ -48,10 +51,10 @@ export default function ProductView ({ params }: { params: { product: string } }
   }
 
   return (
-    <div>
-      <div className='flex pt-4 px-5 gap-4'>
-        <div>
-          <div className='border-2 rounded-2xl overflow-hidden mb-3'>
+    <>
+      <div className='flex pt-4 flex-col px-5 gap-4 sm:flex-row'>
+        <div className='flex gap-2 sm:flex-col'>
+          <div className='border-2 rounded-sm overflow-hidden mb-3'>
             <Image
               src={product?.image[selectedImage] ?? ''}
               alt={product?.name ?? ''}
@@ -60,10 +63,10 @@ export default function ProductView ({ params }: { params: { product: string } }
             />
           </div>
           <div>
-            <div className='flex gap-2'>
+            <div className='flex gap-2 flex-col sm:flex-row'>
               {
                 product?.image.map((image, index) => (
-                  <div key={index} className='border-2 rounded-2xl overflow-hidden cursor-pointer'>
+                  <div key={index} className='border-2 rounded-sm overflow-hidden cursor-pointer'>
                     <Image
                       src={image}
                       alt={product?.name}
@@ -78,7 +81,7 @@ export default function ProductView ({ params }: { params: { product: string } }
           </div>
         </div>
 
-        <div className='flex flex-col w-1/2'>
+        <div className='flex flex-col sm:w-4/5'>
           <h1 className='text-2xl font-semibold text-[--accent-200]'>{product?.name}</h1>
           <Link href={`/brand/${product?.brand._id}`} className='text-[--text-200] font-medium text-base mt-1 mb-3 cursor-pointer'>
             {product?.brand.name}
@@ -91,13 +94,13 @@ export default function ProductView ({ params }: { params: { product: string } }
           <div className='flex gap-2 mt-6 mb-4'>
             {
               product?.options.map((op) => (
-                <button className={`text-[--text-200] pl-3 pr-10 rounded-lg py-2 transition-all select-none flex flex-col
+                <button className={`text-[--text-200] pl-3 pr-10 rounded-sm py-2 transition-all select-none flex flex-col
                   ${op.option === optionSelected?.option
                     ? 'bg-[--accent-100]'
                     : 'bg-[--bg-300] text-[--text-200] hover:bg-[--accent-200] hover:text-[--bg-100]'
                   }`}
                   key={op.option}
-                  onClick={() => { handleWeightChange(op.option) }}
+                  onClick={() => { handleOptionChange(op.option) }}
                 >
                   <div className='font-bold text-sm'>
                     {op.option}
@@ -112,27 +115,28 @@ export default function ProductView ({ params }: { params: { product: string } }
           <hr />
           <div className='flex gap-10 items-center mt-4'>
             <div className='flex gap-1'>
-              <button className={`w-7 h-7 bg-[--bg-300] text-[--text-200] rounded-lg transition-all hover:bg-[--accent-200] hover:text-[--bg-100] flex items-center justify-center ${quantity === 1 ? 'opacity-80 cursor-not-allowed' : ''}`}
+              <button className={`w-7 h-7 bg-[--bg-300] text-[--text-200] rounded-sm transition-all hover:bg-[--accent-200] hover:text-[--bg-100] flex items-center justify-center ${quantity === 1 ? 'opacity-80 cursor-not-allowed' : ''}`}
                 onClick={decreaseQuantity}>
                 -
               </button>
-              <div className='w-7 h-7 bg-[--bg-300] select-none text-[--text-200] rounded-lg transition-all flex items-center justify-center'>
+              <div className='w-7 h-7 bg-[--bg-300] select-none text-[--text-200] rounded-sm transition-all flex items-center justify-center'>
                 {quantity}
               </div>
-              {/* <button className={`w-7 h-7 bg-[--bg-300] text-[--text-200] rounded-lg transition-all hover:bg-[--accent-200] hover:text-[--bg-100] flex items-center justify-center ${quantity === product?.stock ? 'opacity-80 cursor-not-allowed' : ''}`} */}
-                {/* onClick={increaseQuantity}>
+              <button
+                className={`w-7 h-7 bg-[--bg-300] text-[--text-200] rounded-sm transition-all hover:bg-[--accent-200] hover:text-[--bg-100] flex items-center justify-center ${quantity === optionSelected?.stock ? 'opacity-80 cursor-not-allowed' : ''}`}
+                onClick={increaseQuantity}>
                 +
-              </button> */}
+              </button>
             </div>
 
-            <button className='flex items-center font-semibold tracking-wide justify-center gap-2 h-10 w-full bg-[--accent-100] text-[--text-200] rounded-lg transition-all hover:bg-[--accent-200] hover:text-[--bg-100]'>
+            <button className='flex items-center font-semibold tracking-wide justify-center gap-2 h-10 w-full bg-[--accent-100] text-[--text-200] rounded-sm transition-all hover:bg-[--accent-200] hover:text-[--bg-100]'>
               <FaCartPlus />Agregar al carrito
             </button>
           </div>
         </div>
       </div>
 
-      <div className='flex mx-5 my-12 border-2 py-5 px-4 justify-between rounded-md'>
+      <div className='grid grid-cols-2 gap-5 mx-5 my-12 border-2 py-5 px-4 rounded-sm sm:grid-cols-4'>
         <div className='flex text-xs flex-col w-40'>
           <FaTruckArrowRight className='text-4xl text-[--bg-100] bg-[--accent-200] rounded-full w-10 h-10 p-1 mb-2' />
           <p className='font-semibold text-sm'>Envios Gratis</p>
@@ -186,6 +190,6 @@ export default function ProductView ({ params }: { params: { product: string } }
           }
         </Accordion>
       </div>
-    </div>
+    </>
   )
 }
