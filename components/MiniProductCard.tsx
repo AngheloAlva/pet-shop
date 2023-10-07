7c053edb81bf4b88aeb9ddf80afdcf79ebe97bba
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-07 11:31:02 -0300

    fix style

--- components/MiniProductCard.tsx
+++ components/MiniProductCard.tsx
@@ -14,7 +14,7 @@ interface MiniProductCardProps {
 const MiniProductCard = ({ product, setSearchResults }: MiniProductCardProps): JSX.Element => {
   return (
     <Link href={`/product/${product._id}`} onClick={() => { setSearchResults([]) }}>
-      <DialogPrimitive.Close className='flex items-center gap-2 px-3 py-2 hover:bg-[--bg-300] rounded-lg cursor-pointer'>
+      <DialogPrimitive.Close className='flex items-center gap-2 px-3 py-2 hover:bg-[--bg-300] rounded-lg cursor-pointer w-full'>
         <Image src={product.image[0]} alt={product.name} className='w-16 h-16 rounded-lg' width={200} height={200} />
         <div className='flex flex-col items-start'>
           <p className='text-[--text-100] font-medium text-base text-left'>
