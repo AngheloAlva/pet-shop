137ac66e32448d56d33488971cfdd165ee338423
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-06 23:09:48 -0300

    news cancel and success page, and delete al function of createOrder

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -6,13 +6,14 @@ import Link from 'next/link'
 
 const page = (): JSX.Element => {
   return (
-    <div className='flex flex-col items-center py-20 px-10 gap-20'>
-      <h1 className='font-bold text-2xl text-[--text-200] text-center'>
-        Lo sentimos, tu compra no se pudo completar
-      </h1>
-      <Link href={'/'} className='border-2 mb-32 my-16 rounded-sm px-7 py-2 font-semibold text-[--text-100] hover:bg-[--accent-100] transition-colors border-[--accent-100]'>
-        Volver al inicio
-      </Link>
+    <div className='flex items-center justify-center w-screen py-32'>
+      <div className='flex flex-col items-center py-20 border-2 px-7 w-full mx-8 max-w-3xl rounded-sm border-[--bg-300] bg-[--bg-200] text-[--text-200]'>
+        <h1 className='font-bold text-2xl text-center'>Lo sentimos, tu compra no se pudo completar</h1>
+
+        <Link href='/' className='mt-10 border-2 border-[--accent-100] text-[--text-100] font-bold py-2 w-2/4 rounded-sm hover:bg-[--accent-100] hover:text-[--bg-100] text-base text-center transition-colors'>
+          Volver al inicio
+        </Link>
+      </div>
     </div>
   )
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,23 +1,24 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
 
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client'
 
-import { createOrder } from '@/api/order'
-import type { Order } from '@/interfaces/interfaces'
-
 const page = (): JSX.Element => {
-  React.useEffect(() => {
-    const order: Order = {
-    }
-
-    createOrder(order)
-  }, [])
-
   return (
-    <div className='flex flex-col items-center py-8'>
-      <h1 className='font-bold text-2xl text-[--text-200]'>Gracias por la compra</h1>
+    <div className='flex items-center justify-center w-screen py-32'>
+      <div className='flex flex-col items-center py-20 border-2 px-7 w-full mx-8 max-w-3xl rounded-sm border-[--bg-300] bg-[--bg-200] text-[--text-200]'>
+        <h1 className='font-bold text-2xl'>Gracias por la compra</h1>
+
+        <p className='mt-4 text-center'>
+          Tu compra ha sido realizada con éxito. En breve recibirás un correo electrónico con los detalles de tu compra.
+        </p>
+
+        <Link href='/' className='mt-10 border-2 border-[--accent-100] text-[--text-100] font-bold py-2 w-2/4 rounded-sm hover:bg-[--accent-100] hover:text-[--bg-100] text-base text-center transition-colors'>
+          Volver al inicio
+        </Link>
+      </div>
     </div>
   )
 }
