85fd5ff6f99bfa6bf27a22a0b1385f26c20c342b
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-14 21:38:53 -0300

    add new products in to sliders

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,24 @@ import { carouselImages, promotionsImages, servicesImages } from '../data/imgsAr
 import useProductAndBrands from '@/hooks/useProduct&Brands'
 
 export default function Home (): JSX.Element {
-  const { products, brands, userId } = useProductAndBrands({ })
-  const acanaProducts = products.filter(product => product.brand.name === 'Acana')
-  const acanaId = acanaProducts[0]?.brand?._id
+  const { products: allProducts, brands, userId } = useProductAndBrands({ })
+  const { products: acanaProducts } = useProductAndBrands({ brand: '64f2463f5075cf4abe99904b' })
+  const { products: carrierAndBagsProducts } = useProductAndBrands({ category: '65220255842661091b698dc4' })
 
-  const carrierAndBagsProducts = products.filter(product => product.category._id === '6521f6ca842661091b698d40')
-  const carrierAndBagsId = carrierAndBagsProducts[0]?.category?._id
+  const acanaId = acanaProducts[0]?.brand._id
+  const carrierAndBagsId = carrierAndBagsProducts[0]?.category._id
 
   return (
     <>
       <Carousel images={carouselImages} />
 
-      <ProductsSlide slideTitle='Top Ventas' products={products} userId={userId} href='/products' />
+      <ProductsSlide slideTitle='Nuestros Productos' products={allProducts} userId={userId} href='/products' />
 
-      <h2 className='text-xl font-bold text-[--text-100] mb-3 mt-5 pl-5'>Promociones</h2>
-      <div className='grid grid-cols-1 gap-3 px-5 lg:grid-cols-2'>
+      <h2 className='text-lg sm:text-xl font-bold text-[--text-100] mb-3 mt-5 pl-5'>Promociones</h2>
+      <div className='grid grid-cols-1 gap-3 px-5 md:grid-cols-2'>
         {
           promotionsImages.map((image, index) => (
-            <Link href={image.href} key={index} className='rounded-sm overflow-hidden'>
+            <Link href={image.href} key={image.src} className='rounded-sm overflow-hidden'>
               <Image key={index} src={image.src} alt={`Promotion ${index + 1}`} width={1000} height={400} />
             </Link>
           ))
@@ -38,7 +38,8 @@ export default function Home (): JSX.Element {
 
       <ProductsSlide slideTitle='Productos de ACANA' products={acanaProducts} userId={userId} href={`/brand/${acanaId}`} />
 
-      <div className='flex flex-col w-full px-5 my-7 gap-3 lg:flex-row'>
+      <h2 className='text-lg sm:text-xl font-bold text-[--text-100] mb-3 mt-5 pl-5'>Servicios</h2>
+      <div className='flex flex-col w-full px-5 mb-7 gap-3 lg:flex-row'>
         {
           servicesImages.map((image, index) => (
             <Link href={image.href} key={index} className='rounded-sm overflow-hidden shadow-xl'>
@@ -50,7 +51,7 @@ export default function Home (): JSX.Element {
 
       <div className='px-5 flex flex-col gap-3'>
         <div className='flex justify-between items-center'>
-          <h2 className='text-xl font-bold'>Marcas</h2>
+          <h2 className='text-lg sm:text-xl font-bold'>Marcas</h2>
           <Link href='/brand' className='text-[--text-200] px-2 hover:underline py-0 transition-colors font-semibold'>Ver todas</Link>
         </div>
         <BrandsSlider brands={brands} />
