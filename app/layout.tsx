001ec441070cb71f87f07a80a27919e42b12bac5
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-15 19:10:53 -0300

    add new favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import { UserProvider } from '@auth0/nextjs-auth0/client'
 import CartContextContainer from '@/components/AppContainer'
 
 export const metadata: Metadata = {
-  title: 'Pet Shop',
-  description: 'Pet Shop is a demo e-commerce site built with Next.js, Stripe Checkout, Auth0, etc.'
+  title: 'Pet Shop - La mejor tienda online para productos de mascotas',
+  description: 'Pet Shop es una demostración de un sitio e-commerce construido con Next.js, Stripe Checkout, Auth0, etc., ofreciendo una amplia gama de productos de alta calidad para tus mascotas.',
+  keywords: 'Pet Shop, productos para mascotas, tienda online, perros, gatos, camas para mascotas, comida para mascotas, alimento para mascotas, alimento para perros, alimento para gatos, transportadoras para mascotas, Next.js, Stripe Checkout, Auth0, React, TypeScript, Tailwind CSS',
+  robots: 'index,follow'
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
