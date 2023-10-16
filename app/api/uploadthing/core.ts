3bc9eec40b1b22221362c66d548e6fc0f0b550af
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-16 12:05:29 -0300

    fix uploadthing code

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 
 const f = createUploadthing()
@@ -9,7 +10,7 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: '4MB' } })
     .middleware(async ({ req }) => {
       const user = await auth(req)
-      if (!user) throw new Error('Unauthorized')
+      if (user.id === '') throw new Error('Unauthorized')
       return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {
